972bf135ff648a59891325c8ef3709a436c66857
Faye Rodgers <fr7@mib110555i.internal.sanger.ac.uk>
2018-12-05 15:07:52 +0000

    Rename cell types

--- exp2_CCA_raw.R
+++ exp2_CCA_raw.R
@@ -56,18 +56,17 @@ for (sample in samples){
 }
 
 #Run CCA
-experiment<- RunMultiCCA(samples, genes.use = genes.use, num.ccs = 30)
+experiment<- RunMultiCCA(samples, genes.use = genes.use, num.ccs = 40)
 pdf(paste0(out,"cc_saturation_raw.pdf"))
-p1 <- MetageneBicorPlot(experiment, grouping.var = "infection_status", dims.eval = 1:40, display.progress = FALSE) #use this plot to decide how many CCs to include downstream. Choose 15.
-p1
+p1 <- MetageneBicorPlot(experiment, grouping.var = "infection_status", dims.eval = 1:40, display.progress = FALSE) #use this plot to decide how many CCs to include downstream. Choose 30.
 dev.off()
 
 #STOP HERE! Decide how many CCs to include in the analysis.
 ################
 
-experiment <- AlignSubspace(experiment, reduction.type = "cca", grouping.var = "infection_status", dims.align = 1:15)
-experiment <- FindClusters(experiment, reduction.type = "cca.aligned", resolution = 1, dims.use = 1:15)
-experiment <- RunTSNE(experiment, reduction.use = "cca.aligned", dims.use = 1:15, do.fast = T)
+experiment <- AlignSubspace(experiment, reduction.type = "cca", grouping.var = "infection_status", dims.align = 1:30)
+experiment <- FindClusters(experiment, reduction.type = "cca.aligned", resolution = 1, dims.use = 1:30) #Might want to play with the resolution.
+experiment <- RunTSNE(experiment, reduction.use = "cca.aligned", dims.use = 1:30, do.fast = T)
 pdf(paste0(out,'tsne_infection_status_raw.pdf'))
 p2 <- TSNEPlot(experiment, do.return = T, pt.size = 1.5, group.by = "infection_status")
 p2
@@ -77,7 +76,7 @@ p3 <- TSNEPlot(experiment, do.label = T, do.return = T, pt.size = 1.5)
 p3
 dev.off()
 pdf(paste0(out,'markers_raw.pdf'))
-p4 <- FeaturePlot(experiment, features.plot = c("Alpi","Muc2","Chga","Lgr5","Dclk1","Cdk4","Il33","Prdx6","Rps26"), min.cutoff="q9", cols.use = c("lightgrey", "blue"), pt.size = 1.5)
+p4 <- FeaturePlot(experiment, features.plot = c("Alpi","Muc2","Chga","Lgr5","Dclk1","Cdk4","Il33","Prdx6","Rps26"), min.cutoff="q9", cols.use = c("lightgrey", "blue"), pt.size = 1.5) 
 p4
 dev.off()
 
@@ -85,8 +84,8 @@ dev.off()
 #Stop here! Examine the clusters to identify cell types
 #rename cells according to identified clusters
 
-new.ident<-c("ENTEROCYTE_A","TA_A","ENTEROCYTE_B","GOBLET","ENTEROCYTE_C","ENTEROCYTE_D","TA_B")
-for (i in 0:6){ experiment<-RenameIdent(experiment,old.ident.name = i,new.ident.name = new.ident[i+1])}
+new.ident<-c("ENTEROCYTE_A","ENTEROCYTE_B","ENTEROCYTE_C","TA_A","ENTEROCYTE_D","GOBLET","TA_B","ENTEROCYTE_E","ENTEROCYTE_F","TUFT")#This will change depending on the clusters identified.
+for (i in 0:9){ experiment<-RenameIdent(experiment,old.ident.name = i,new.ident.name = new.ident[i+1])}
 #Stash the new identities as 'cell_type'
 experiment<-StashIdent(experiment,save.name="cell_type")
 pdf(paste0(out,'named_tsne_clusters_raw.pdf'))
@@ -106,7 +105,7 @@ p6
 dev.off()
 
 #Get cell numbers for each cluster
-cell_types<-c("ENTEROCYTE_A","ENTEROCYTE_B","ENTEROCYTE_C","ENTEROCYTE_D","TA_A","TA_B","GOBLET")
+cell_types<-c("ENTEROCYTE_A","ENTEROCYTE_B","ENTEROCYTE_C","ENTEROCYTE_D","ENTEROCYTE_E","ENTEROCYTE_F","TA_A","TA_B","GOBLET","TUFT") 
 control<-c()
 infected<-c()
 control_percent<-c()
@@ -114,9 +113,9 @@ infected_percent<-c()
 control_sum = nrow(experiment@meta.data[which(experiment@meta.data$infection_status=='CONTROL'),])
 infected_sum = nrow(experiment@meta.data[which(experiment@meta.data$infection_status=='INFECTED'),])
 for (cell_type in cell_types){
-  x = nrow(experimentw@meta.data[which(experiment@meta.data$cell_type==cell_type & experiment@meta.data$infection_status=='CONTROL'),])###HERE
+  x = nrow(experiment@meta.data[which(experiment@meta.data$cell_type==cell_type & experiment@meta.data$infection_status=='CONTROL'),])
   x_percent = round((x*100/control_sum),1)
-  y = nrow(experiment_1_raw@meta.data[which(experiment_1_raw@meta.data$cell_type==cell_type & experiment_1_raw@meta.data$infection_status=='INFECTED'),])
+  y = nrow(experiment@meta.data[which(experiment@meta.data$cell_type==cell_type & experiment@meta.data$infection_status=='INFECTED'),])
   y_percent = round((y*100/infected_sum),1)
   control<-c(control,x)
   infected<-c(infected,y)
